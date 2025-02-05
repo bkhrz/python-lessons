3ae16cd788fb3ba269259e61dd3adde3a0300f72
bkhrz <behruzqobilov26@gmail.com>
2025-02-05 09:20:23 +0500

    First lesson

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+#a = input("Your age:")
+#print(a)
+
+# n = 73.4842248763
+# print(f"{n:.5f}")
+
+# a = "Hello World"
+# print(a[-2:0])
+
+word = input("Enter a word: ")
+#last_character = word[-1]
+print(f"Last character is {word[-1]}")
