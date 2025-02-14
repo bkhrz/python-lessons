5a28e361a482d1e8f34acc9b5dcdfb16bc4d41c7
bkhrz <behruzqobilov26@gmail.com>
2025-02-14 19:45:27 +0500

    lesson 1

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -51,11 +51,11 @@ from itertools import count
 # s1 = {"a", "b", 'c'}
 # print(s1.pop())
 
-for i in range(2,100,1):
-    a = True
-    for j in range(2,i//2+1,1):
-        if i % j == 0:
-            a = False
-            break
-    if a:
-        print(i)
+# for i in range(2,100,1):
+#     a = True
+#     for j in range(2,i//2+1,1):
+#         if i % j == 0:
+#             a = False
+#             break
+#     if a:
+#         print(i)
