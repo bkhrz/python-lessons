352ff53ded64da23427577847e472bf05114705f
Behruz Qobilov <behruzqobilov26@gmail.com>
2025-02-18 20:36:54 +0500

    lesson 2

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -59,3 +59,14 @@ from itertools import count
 #             break
 #     if a:
 #         print(i)
+
+# def invest(amount, rate, years):
+#     for i in range(1, years+1, 1):
+#         amount = round(amount * (1+ rate), 2)
+#         print(f"year {i}: ${amount}")
+#
+# amount = int(input())
+# rate = float(input("Rate in percent: %")) * (1/100)
+# years = int(input())
+# invest(amount, rate, years)
+
