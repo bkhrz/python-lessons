c79aa89c7b1f64515aaf86d90fab39f3486c0360
Behruz Qobilov <behruzqobilov26@gmail.com>
2025-03-13 10:28:27 +0500

    not necy

--- lesson-14/homework/task2.py
+++ lesson-14/homework/task2.py
@@ -4,7 +4,7 @@ import numpy as np
 def power_of_number(number: int, exponent: int) -> int:
     return number**exponent
 
-arr_of_numbers = [2, 3, 4, 5]
-arr_off_exponents = [1, 2, 3, 4]
+arr_of_numbers = np.array([2, 3, 4, 5])
+arr_off_exponents = np.array([1, 2, 3, 4])
 
 print(power_of_number(arr_of_numbers, arr_off_exponents))
